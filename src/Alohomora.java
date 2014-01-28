cecb664cdb837adf419ce7d5ee93911dd7e0128e
miriamposner <miriam.posner@gmail.com>
2014-01-28 15:08:27 -0800

    Changed the java function

--- src/Alohomora.java
+++ src/Alohomora.java
@@ -2,6 +2,10 @@ package edu.hogwarts.SpellSim.Spells;
 
 class Alohomora implements Spell {
     public String name = "Alohomora";
+    
+    public String getName () {
+    	return name;
+    }
 
     public void invokeOn(Subject subject) {
         throw new NotImplementedError("Not yet implemented.");
